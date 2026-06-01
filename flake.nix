6b6201263cb2fc2951a781db7577f3092da06a95
jrrom <77691121+jrrom@users.noreply.github.com>
2026-06-01 06:57:34 +0530

    fix: re-add pkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,9 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   outputs = { self, nixpkgs, ... }:
-    {
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
       packages.x86_64-linux.default = pkgs.callPackage ./default.nix { };
       nixosModules.default = import ./nixos-module.nix;
     };
