40a47d843b9e8e5205f1bff5333d1a1ef936c8e0
jrrom <77691121+jrrom@users.noreply.github.com>
2026-07-29 23:40:56 +0530

    update: more idiomatic

--- nixos-module.nix
+++ nixos-module.nix
@@ -1,11 +1,13 @@
-{ config, lib, pkgs, inputs, ... }:
-
 {
-  options.programs.kDrive.enable = lib.mkEnableOption "kDrive";
+  outputs = { self, nixpkgs, ... }: {
+    nixosModules.default = { config, lib, pkgs, ... }: {
+      options.programs.kDrive.enable = lib.mkEnableOption "kDrive";
 
-  config = lib.mkIf config.programs.kDrive.enable {
-    environment.systemPackages = [
-      inputs.kDrive.packages.${pkgs.system}.default
-    ];
+      config = lib.mkIf config.programs.kDrive.enable {
+        environment.systemPackages = [
+          self.packages.${pkgs.system}.default
+        ];
+      };
+    };
   };
 }
